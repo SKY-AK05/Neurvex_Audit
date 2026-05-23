6826f7b6608f2ff7f7462e8215c22937db5d1674
SKY-AK05 <aakash20030501@gmail.com>
2026-05-24 00:45:37 +0530

    Remove Share Audit from navbar, fix Settings width, hardcode audit URL

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -29,11 +29,6 @@
             <img src="/logo_orchvate.png" alt="Orchvate" class="powered-logo" />
           </div>
 
-          <button class="share-audit-btn" @click="copyAuditLink" :title="shareCopied ? 'Copied!' : 'Copy audit form link'">
-            <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>
-            {{ shareCopied ? 'Copied!' : 'Share Audit' }}
-          </button>
-
           <div class="bell-wrap" ref="bellWrap">
             <button
               type="button"
